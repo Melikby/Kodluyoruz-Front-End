c2b8dfa747af154bedbf1fabb8afd8fe8be2aea5
Melikby <melik215666@gmail.com>
2022-11-15 16:18:22 +0300

    Js-ödev-1

---
+++ JavaScript/Ödev-1/clock.js
@@ -0,0 +1,23 @@
+let user=prompt("Adınızı Giriniz:")
+let text = document.querySelector("#myName")
+text.innerHTML=user
+
+function saatGoster() {
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+
+    const gunler = ["Pazar", "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi"];
+    const g = new Date();
+    let gun = gunler[g.getDay()];
+
+    hours = (hours < 10) ? "0" + hours : hours;
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+
+    let time = hours + ":" + minutes + ":" + seconds + " " + gun
+    document.getElementById('myClock').innerHTML = time;
+}
+setInterval(saatGoster, 1000);
